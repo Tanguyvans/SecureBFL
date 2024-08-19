e6907b3683664bcd57241d53843b1dbf2e453820
lcoEntreprise <leandrocollier@outlook.be>
2024-08-19 11:00:38 +0200

    update evaluation last global_model

--- model_evaluation.py
+++ model_evaluation.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 import torch
 import json
@@ -15,6 +16,19 @@ def get_model_files(dir_model, training_approach="CFL"):
     return model_files
 
 
+def get_global_model_storage_reference(file_path):
+    with open(file_path, 'r') as ff:
+        content = ff.read()
+
+    # Regex (Regular expression) to extract blocks with the global model
+    pattern = r'(?s)model type:\s*global_model.*?storage reference:\s*(\S+)'
+
+    # Search for all matches
+    matches = re.findall(pattern, content)
+
+    return matches[-1] if matches else None
+
+
 # %%
 if __name__ == '__main__':
     # %%
@@ -22,6 +36,7 @@ if __name__ == '__main__':
     training_approach = "BFL"  # "CFL" # "BFL"  # scratch
     matrix_path = "matrix"
     roc_path = "roc"
+    path_nodetxt = "results/BFL/node1.txt"
     device = "mps"
     with open(f"results/{training_approach}/config.json", "r") as f:
         config = json.load(f)['settings']
@@ -64,16 +79,19 @@ if __name__ == '__main__':
 
     for model_file in model_list:
         if model_file.endswith('.npz'):
+            print(model_file)
             loaded_weights_dict = np.load(config['save_model'] + model_file, allow_pickle=True)
         else:
             # Torch model
             loaded_weights_dict = torch.load(config['save_model'] + model_file)
 
+        print("get the weights")
         loaded_weights = [val for key, val in loaded_weights_dict.items() if 'len_dataset' not in key]
 
         # Evaluate the model
+        print("evaluate the model")
         metrics = flower_client.evaluate(loaded_weights, {'name': 'global_test_model_file_'})
-
+        print("model evaluated")
         if model_file[0:2] == "n1" or training_approach == "scratch":
             model_file = model_file[2:]
 
@@ -83,11 +101,21 @@ if __name__ == '__main__':
     os.makedirs(config['save_results'], exist_ok=True)
     if training_approach == "scratch":
         evaluation.sort(key=lambda x: int(x[0].split('.')[0].split("_")[-1]))
+
     elif training_approach == "BFL": 
         evaluation.sort(key=lambda x: int(x[0][1:].split("_")[0]))
+
     else:
         evaluation.sort(key=lambda x: int(x[0][1:].split('.')[0]))
 
     with open(config['save_results'] + "evaluation.txt", "w") as f:
         for model_file, loss, acc in evaluation:
             f.write(f"{model_file}: {loss}, {acc} \n")
+
+    # %%
+    model_file = get_global_model_storage_reference(path_nodetxt)  # model_file = "m80_1723036980_bd6437ad.npz"
+
+    loaded_weights_dict = np.load(model_file, allow_pickle=True)
+    loaded_weights = [val for key, val in loaded_weights_dict.items() if 'len_dataset' not in key]
+    metrics = flower_client.evaluate(loaded_weights, {'name': 'global_test_model_file_best_'})
+    print(metrics)
