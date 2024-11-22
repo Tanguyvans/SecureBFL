85b9cae3a8bb01cc1fdfba7fa1fefc433df9f064
Tanguy VANSNICK <191451@umons.ac.be>
2024-11-23 00:13:48 +0100

    coef

--- node.py
+++ node.py
@@ -82,12 +82,12 @@ def start_server(host, port, handle_message, num_node):
 
 
 class Node:
-    def __init__(self, id, host, port, consensus_protocol, test, save_results, coef_usefull=1.01, tolerance_ceil=0.1,
+    def __init__(self, id, host, port, consensus_protocol, test, save_results, coef_useful=1.01, tolerance_ceil=0.1,
                  ss_type="additif", m=3, **kwargs):
         self.id = id
         self.host = host
         self.port = port
-        self.coef_usefull = coef_usefull
+        self.coef_useful = coef_useful
         self.tolerance_ceil = tolerance_ceil
 
         self.peers = {}
@@ -200,7 +200,7 @@ class Node:
 
         print(f"node: {self.id} update_eval: {update_eval} gm_eval: {gm_eval}")
 
-        allowed_unimprovement = min(self.tolerance_ceil, gm_eval[0] * (self.coef_usefull - 1))
+        allowed_unimprovement = min(self.tolerance_ceil, gm_eval[0] * (self.coef_useful - 1))
         if update_eval[0] <= gm_eval[0] + allowed_unimprovement:
             return True
         else: 
