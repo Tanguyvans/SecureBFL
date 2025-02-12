e49ddd648ca05d899fa1a38e8bcc9af5c3c46a7e
Tanguyvans <71881765+Tanguyvans@users.noreply.github.com>
2025-02-12 14:17:42 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 settings = {
-    "name_dataset": "cifar10",  # "cifar10" or "cifar100" or "caltech256"
-    "arch": "mobilenet",  # "mobilenet" or "resnet18"
+    "name_dataset": "cifar100",  # "cifar10" or "cifar100" or "caltech256"
+    "arch": "shufflenet",  # "mobilenet" or "resnet18" or "shufflenet"
     "pretrained": True,
     "patience": 3,
     "batch_size": 32,
     "n_epochs": 15,
     "number_of_nodes": 3,
-    "number_of_clients_per_node": 6,
+    "number_of_clients_per_node": 15,
     "min_number_of_clients_in_cluster": 3,
 
     "check_usefulness": False,
     "coef_useful": 1.05,   # 1.05
     "tolerance_ceil": 0.08,
 
-    "poisoned_number": 6,
+    "poisoned_number": 15,
     "n_rounds": 50,
     "choice_loss": "cross_entropy",
     "choice_optimizer": "Adam",
